8837e42dc457c8950aa946134680fa16ba6d1b04
back-spac3 <backspacecrew18@gmail.com>
2021-09-14 00:30:26 +0600

    First Commit 🚀

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express'),
     mongoose = require('mongoose'),
     cors = require('cors'),
     bodyParser = require('body-parser'),
+    path = require('path')
     app = express(),
     mongourl = process.env.MONGO_CONNECTION_URL;
 
@@ -24,6 +25,13 @@ app.use('/public', express.static('public'));
 app.use('/api', addDocument)
 app.use('/api', sendEmail)
 app.use('/api', downloadFile)
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(process.env.PORT || 3001, () => {
     console.log('lisening on port 3001');
