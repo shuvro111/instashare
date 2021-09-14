9b02e7b1caf5307c825317cbe77e1b146df66328
back-spac3 <backspacecrew18@gmail.com>
2021-09-14 19:30:42 +0600

    First Commit 🚀

--- routes/download.js
+++ routes/download.js
@@ -7,13 +7,11 @@ router.get('/documents/download/:uuid', async (req, res) => {
     if(!file){
         return res.json('Whoops! File has been removed or link expired')
     }
-    return res;
 
-    // const filePath = `${(__dirname)}/../${file.path}`;
-    // console.log(filePath);
-    // const fileName = file.documentName;
-    // console.log(fileName)
-    // return res.download(filePath, fileName);
+    const response = await file.save();
+    const filePath = `${(__dirname)}/../${file.path}`;
+    // return res.download(filePath);
+    return res.json(filePath)
 
 });
 
